6e4c5cc138c23ece61fdcc5f383967bfe8150cdc
Chaewon Yoon <cyoon99@Chaewons-MacBook-Air.local>
2022-11-12 17:24:28 -0600

    landing page

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,24 @@ import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div className="static">
-      <Navbar />
-      <div className="absolute right-0 h-screen w-1/3 bg-primary-green">
-        
+    <>
+      <Navbar></Navbar>
+      <div className="text-7xl indent-20 text-lime-600 font-semibold">
+      <p>Customer Analytics </p>
+      <p>Beautified.</p>
       </div>
-    </div>
+
+      <div className="text-3xl indent-20 text-lime-900">
+      <p>Discover a powerful sentiment </p>
+      <p>analysis tool for your reviews in one click</p>
+      </div>
+
+      
+      <p className="bg-primary-green font-medium text-md text-white px-6 py-4 rounded-lg"> 
+      Upload CSV
+      </p>
+      
+      
+   </>
   )
 }
