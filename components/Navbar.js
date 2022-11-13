98a85ea70eca3c114b64162512ac50cdd63759dd
Abhishek <abhimore74@gmail.com>
2022-11-12 18:25:49 -0600

    nav and landing

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center px-16 pt-8">
-      <p className="text-2xl font-OP text-primary-green font-bold">ReVal</p>
-      <a className="bg-primary-green font-medium text-md text-white px-6 py-4 rounded-lg">View on Github</a>
+      <Link href='/' className="text-2xl font-OP text-primary-green font-bold">ReVal</Link>
+      <a href="https://github.com/Abhishek-More/reval" target="_blank" className="bg-primary-green font-medium text-md text-white px-6 py-4 rounded-lg">View on Github</a>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,24 @@ export default function Home() {
     <>
       <Navbar></Navbar>
       <div className="absolute right-0 top-0 -z-50 w-1/3 h-screen bg-gradient-to-br from-[#C4EFC5] to-[#E2EDA7]"></div>
-      <div className="text-7xl indent-20 text-lime-600 font-semibold">
-      <p>Customer Analytics </p>
-      <p>Beautified.</p>
-      </div>
 
-      <div className="text-3xl ml-16 text-lime-900">
-        <p>Discover a powerful sentiment </p>
-        <p>analysis tool for your reviews in one click</p>
-      </div>
+      <div className="ml-32 mt-32">
+        <div className="text-7xl text-lime-600 font-semibold">
+        <p>Customer Analytics </p>
+        <p>Beautified.</p>
+        </div>
 
-      
-      {/* <p className="ml-16 bg-primary-green font-medium text-md text-white l-[100px] w-[150px] rounded-lg">Upload CSV</p> */}
-      <div className="flex justify-between w-2/5 items-center px-16 pt-8 mt-8">
-      <a className="bg-primary-green font-medium text-md text-white px-16 py-4 rounded-lg">Upload CSV</a>
-      <a className="bg-white border-2 border-primary-green font-medium placeholder:text-md text-primary-green px-16 py-4 rounded-lg">Review Data</a>
+        <div className="text-3xl mt-6 text-lime-900">
+          <p>Discover a powerful sentiment </p>
+          <p>analysis tool for your reviews in one click</p>
+        </div>
+
+        
+        {/* <p className="ml-16 bg-primary-green font-medium text-md text-white l-[100px] w-[150px] rounded-lg">Upload CSV</p> */}
+        <div className="flex w-2/5 items-center pt-8 mt-10">
+        <a className="bg-primary-green font-medium text-md text-white px-16 py-4 rounded-lg">Upload CSV</a>
+        <a className="bg-white border-2 ml-8 border-primary-green font-medium placeholder:text-md text-primary-green px-16 py-4 rounded-lg">Review Data</a>
+        </div>
       </div>
       
    </>
